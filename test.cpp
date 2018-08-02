ec9cfda8ee7f90484d61eb5252c3b62ecb81a630
DoraLC <34760377+DoraLC@users.noreply.github.com>
2018-08-02 13:51:32 +0800

    update

--- test.cpp
+++ test.cpp
@@ -2,7 +2,8 @@
 using namespace pxt;
 
 namespace test{
-  //%
+  //%block=sum %x| + %y blockId="sum"
+  //%shim=test::number_sum
   int number_sum(int x, int y){
     return x+y;
   }
--- test.ts
+++ test.ts
@@ -1,5 +1,6 @@
+//%block=ccptest
 declare namespace test{
-  //%block=sum %x|%y blockId="sum"
+  //%block=sum %x| + %y blockId="sum"
   //%shim=test::number_sum
   function number_sum(x: number, y: number): number;
 }
