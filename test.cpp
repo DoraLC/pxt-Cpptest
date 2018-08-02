8455c19af476213ad2a1322890a2f1a29a0743fc
DoraLC <34760377+DoraLC@users.noreply.github.com>
2018-08-02 14:06:34 +0800

    update

--- test.cpp
+++ test.cpp
@@ -4,7 +4,7 @@ using namespace pxt;
 namespace test{
   //%block=sum %x| + %y blockId="sum"
   //%shim=test::number_sum
-  float number_sum(int x, int y){
-    return x/y;
+  int number_sum(int x, int y){
+    return x + y;
   }
 }
