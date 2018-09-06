aa7d9444042a0345724bdc467f402adca22669e7
DoraLC <34760377+DoraLC@users.noreply.github.com>
2018-09-06 12:10:55 +0800

    update

--- test.ts
+++ test.ts
@@ -1,6 +1,8 @@
 //% color=#F6421B icon="\uf121" block="cpptest"
-declare namespace test{
+namespace test{
   //%block="sum %x| + %y" blockId=sum
   //%shim=test::number_sum
-  function number_sum(x: number, y: number): number;
+  export function number_sum(x: number, y: number): number{
+  return 0
+  }
 }
